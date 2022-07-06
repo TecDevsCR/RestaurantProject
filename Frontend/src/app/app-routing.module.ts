ee5a2614974873bfb79d71897c9186195fec2618
Josue Santamaria <48070852+Santamix728@users.noreply.github.com>
2022-07-06 14:35:54 -0600

    Reserve link page

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { ReserveComponent } from './components/reserve/reserve.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'reserve', component: ReserveComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'login' },
 ];
 
